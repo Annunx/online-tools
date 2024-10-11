cad64541d1eae1cf81a44264c4c845d1b570e789
方甲云 <annunx@126.com>
2024-10-11 23:06:46 +0800

    feat: 更新首页

--- scripts/index.js
+++ scripts/index.js
@@ -11,8 +11,12 @@ const app = new Vue({
   el: '#root',
   data: {
     apps: [],
+    styleObj: {
+      paddingBottom: "80px"
+    }
   },
   mounted: function () {
+    window.addEventListener("resize", this.onResize);
     this.initData();
   },
   methods: {
@@ -26,6 +30,12 @@ const app = new Vue({
     handleOpen(app){
       let target = app.target ? app.target : '_blank';
       window.open(app.index, target);
+    },
+    onResize() {
+      const height = document.getElementById("footer").clientHeight;
+      this.styleObj.paddingBottom = `${height + 20}px`;
+      console.log(this.styleObj.paddingBottom);
+      
     }
   },
 })
